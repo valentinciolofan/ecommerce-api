cc2ed724e99f037462296f653a6f0c95206c4df4
valentinciolofan <valentincatalin02@gmail.com>
2024-04-09 22:11:22 +0300

    added session management for login

--- server.js
+++ server.js
@@ -2,47 +2,87 @@ import express from 'express';
 import knex from 'knex';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
+import session from 'express-session';
+import connectPgSimple from 'connect-pg-simple';
 const app = express();
 const port = 3000;
 
+
+app.use(session({
+  secret: 'your_secret_key', // A secret key for session encoding
+  httpOnly: false,
+  resave: false,              // Forces the session to be saved back to the session store
+  saveUninitialized: true,    // Forces a session that is "uninitialized" to be saved to the store
+  cookie: { maxAge: 3600000, secure: false }   // Note: secure: true should be used in production with HTTPS
+}));
+
 app.use(express.json());
-
-
 app.use(cors({
-  origin: 'http://localhost:4321'
+  origin: 'http://localhost:4321',
+  credentials: true
 }));
 
 
 
-
-
-
-app.post('/login', async (req, res) => {
-  const { email } = req.body;
-  console.log(req.body);
-
-  bcrypt.compare(x, '$2b$10$1AmGO19ntNKbINVrwYxRwuDr0wGjUxQX99OcM2WXVW6mb0U.THini').then(function (result) {
-    console.log(result);
+app.get('/', (req, res) => {
+  res.json({
+    "userId": req.session.userId,
+    "userSessionId": req.session.id
   });
+});
 
-
-  try {
-    const users = await knex.select('*').from('users');
-    const user = users.find(user => user.email === email.toLowerCase());
-
-    console.log(users);
-    console.log(user)
-    if (user) {
-      res.send("Logged In");
-    } else {
-      res.send("Try again");
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server error");
+app.get('/shop', (req, res) => {
+  if (req.session.userId) {
+    res.send(`You are logged in as ${req.session.userId}`)
+  } else {
+    res.send(`You aren't logged in`);
+    
   }
 });
 
+app.get('/test-session', (req, res) => {
+  if (req.session.views) {
+    req.session.views++;
+  } else {
+    req.session.views = 1;
+  }
+  res.status(200).send(`Views: ${req.session.views}`);
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+     knex
+      .select('*')
+      .from('login')
+      .where('email', '=', email)
+      .then(response => {
+        bcrypt.compare(password, response[0].hash).then(function (result) {
+          return result;
+        }).then(isValid => {
+          if (isValid) {
+            req.session.userId = response[0].id;
+            return res.status(200).json({ message: 'Logged in!', redirect: '/shop' });
+
+          }
+          res.status(401).send('User or password could be wrong. Try again!');
+        });
+      });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+
+app.get('/profile', (req, res) => {
+  console.log(req.session.userId);
+  res.json({
+    "userId": req.session.userId,
+    "userSessionId": req.session.id
+  });
+  
+});
 
 
 app.post('/register', async (req, res) => {
@@ -74,12 +114,9 @@ app.post('/register', async (req, res) => {
     .catch(err => {
       console.log('Error:' + err);
     });
-newUser.then(user => {
-  res.send(user[0]);
-});
-
-
-
+  newUser.then(user => {
+    res.send(user[0]);
+  });
 });
 
 
