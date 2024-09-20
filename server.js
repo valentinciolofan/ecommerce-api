3af74261975d7212f11ceceb5e39127e415db687
valentinciolofan <valentincatalin02@gmail.com>
2024-09-20 16:24:15 +0300

    allow headers

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(express.json());
 app.use(cors({
   origin: 'http://localhost:4321',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(session({
   secret: 'your_secret_key', // A secret key for session encoding
