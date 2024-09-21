9838cfb375a42f7adcfdbcd327c7e066e9a453ca
valentinciolofan <valentincatalin02@gmail.com>
2024-09-21 22:06:41 +0300

    .

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ app.use(
     saveUninitialized: false, // Only save sessions that are initialized
     cookie: {
       maxAge: 3600000, // 1 hour session expiration
-      secure: false, // Ensure cookies are only sent over HTTPS in production
+      secure: true, // Ensure cookies are only sent over HTTPS in production
       httpOnly: true, // Make cookie inaccessible to JavaScript
-      sameSite: 'None', // Prevent CSRF by only sending cookies on same-site requests
+      sameSite: 'none', // Prevent CSRF by only sending cookies on same-site requests
     },
   })
 );
